375ada7d4b4ea34d128e973494de4d739cb11470
JamaicanMoose <jackcollinsnj@gmail.com>
2020-04-02 00:59:36 -0400

    feature: scrolling bg, train mvmt, basic char
    - added scrolling background based on local player's train movment
    - added local player character
    - added relative train movement for rival trains
    - added DarkRift
    - added placeholder assets

---
+++ train-to-somewhere/Assets/Resources/Scripts/LocalPlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalPlayerController : MonoBehaviour
+{
+    public float moveSpeed = 2.5f;
+    CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection *= moveSpeed;
+        if (Input.GetButtonDown("Jump"))
+            moveDirection *= 100f;
+        characterController.SimpleMove(moveDirection);
+    }
+}
---
+++ train-to-somewhere/Assets/Resources/Scripts/TrainController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 2f;
+    public float slowdownRate = .1f;
+    public ScrollingBackground backgroundController;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (speed > 0)
+            speed -= slowdownRate * Time.deltaTime;
+        else
+            speed = 0;
+        backgroundController.bgSpeed = speed;
+    }
+}
