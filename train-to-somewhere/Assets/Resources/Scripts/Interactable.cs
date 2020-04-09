af88b327458c24e019e2b50e106b25d82b4522c8
JamaicanMoose <jackcollinsnj@gmail.com>
2020-04-09 15:20:49 -0400

    feature: better state tracking for interactables
    - interactables now track who is interacting with them
    - interactables now have a network unique id
    - more interactable state now tracked in hooks rather than by playerinteract

--- train-to-somewhere/Assets/Resources/Scripts/Interactable.cs
+++ train-to-somewhere/Assets/Resources/Scripts/Interactable.cs
@@ -1,19 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
 
 public class Interactable : MonoBehaviour
 {
     public bool inUse = false;
     public float holdTime = 2.0f;
+    public Transform interactingPlayerTransform;
     public UnityEvent startUse = new UnityEvent();
     public UnityEvent duringUse = new UnityEvent();
     public UnityEvent afterUse = new UnityEvent();
     public UnityEvent abortUse = new UnityEvent();
 
+    public void Awake()
+    {
+        if (gameObject.GetComponent<NetworkTrackable>() == null)
+        {
+            NetworkTrackable trackable = gameObject.AddComponent<NetworkTrackable>() as NetworkTrackable;
+            trackable.uniqueID = System.Guid.NewGuid().ToString();
+        }
+    }
+
     public void StartUse()
     {
+        inUse = true;
         if (startUse != null)
         {
             startUse.Invoke();
@@ -30,17 +39,21 @@ public class Interactable : MonoBehaviour
 
     public void AfterUse()
     {
+        inUse = false;
         if (afterUse != null)
         {
             afterUse.Invoke();
         }
+        interactingPlayerTransform = null;
     }
 
     public void AbortUse()
     {
+        inUse = false;
         if (abortUse != null)
         {
             abortUse.Invoke();
         }
+        interactingPlayerTransform = null;
     }
 }
---
+++ train-to-somewhere/Assets/Resources/Scripts/NetworkTrackable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+// A game object that should be identifiable across clients.
+public class NetworkTrackable : MonoBehaviour
+{
+    public string uniqueID;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(uniqueID, $"{gameObject.name}: NetworkTrackables must have a unique ID.");
+    }
+}
