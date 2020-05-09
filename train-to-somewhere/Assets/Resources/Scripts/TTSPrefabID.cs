24105b1701514d0e2139b458477d80e5746b4b7f
RussellRuffolo <ruffolorussell@gmail.com>
2020-05-09 12:24:51 -0400

    Removed workaround in ttsprefabid

--- train-to-somewhere/Assets/Resources/Scripts/TTSPrefabID.cs
+++ train-to-somewhere/Assets/Resources/Scripts/TTSPrefabID.cs
@@ -7,26 +7,6 @@ public class TTSPrefabID : MonoBehaviour
     public string prefabID;
     private bool isServer;
 
-    private void Awake()
-    {
-        isServer = GameObject.FindGameObjectWithTag("Network")
-            .GetComponent<DarkRift.Server.Unity.XmlUnityServer>() != null;
-        if(isServer)
-        {
-            if (GetComponent<PickupGeneric>() != null)
-            {
-                Transform trainTransform = GameObject.FindGameObjectWithTag("Train").transform;
-          
-                transform.parent = trainTransform;
-
-                GetComponent<TTSID>().Init();
-        
-            }
-            
-            
-        }
-    }
-
     public static string Get(GameObject go)
     {
         TTSPrefabID p = go.GetComponent<TTSPrefabID>();
